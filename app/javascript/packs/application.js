6dbe454520f09a467897efefc7f0e8dde3eaf3a7
kazuma-soon <podradio2020@gmail.com>
2022-03-02 02:58:28 +0900

    Fix: ドラッグをHammerjsでスマホ対応

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,7 @@
 import "bootstrap";
 import '@fortawesome/fontawesome-free/js/all';
 import "../stylesheets/application";
+import 'hammerjs';
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -13,7 +14,6 @@ import "channels"
 
 require("src/goals_add_elements");
 require("src/leaves_add_elements");
-require("src/smartphone_drag");
 require("src/drag");
 
 const images = require.context('../images', true)
--- app/javascript/src/drag.js
+++ app/javascript/src/drag.js
@@ -1,26 +1,46 @@
-//葉っぱのドラッグ機能
-
-function draggable(target) {
-  target.onmousedown = function() {
-    document.onmousemove = mouseMove;
-  };
-  document.onmouseup = function() {
-    document.onmousemove = null;
-  };
-  function mouseMove(e) {
-    target.style.top = e.y + 'px';
-    target.style.left = e.x + 'px';
-  }
-}
-
-const leaves = document.getElementsByClassName('drag');
-
-//ページ読み込み完了を待つ
 window.addEventListener('load', function(){
 
+  const leaves = document.getElementsByClassName('drag');
+
   for(let i = 0; i < leaves.length; i++) {
-    draggable(leaves[i]); 
-  }
+    let leaf = leaves[i];
+    let hammerLeaf = new Hammer(leaf);
+    hammerLeaf.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+    
+    // 葉っぱの初期位置を取得（不変の値）
+    const defaultX = window.innerWidth * parseInt(leaf.style.left) / 100;
+    const defaultY = window.innerHeight* parseInt(leaf.style.top)  / 100;
 
-});
+    hammerLeaf.on('pan', function(e) {
+      console.log(window.innerWidth * parseInt(leaf.style.top) / 100);
+      leaf.style.left = defaultX + e.deltaX + 'px';
+      leaf.style.top =  defaultY + e.deltaY + 'px';
+    });
+  } 
+})
 
+////葉っぱのドラッグ機能
+//
+//function draggable(target) {
+//  target.ontouchstart = function() {
+//    document.ontouchmove = fingerMove;
+//  };
+//  document.onmouseup = function() {
+//    document.ontouchmove = null;
+//  };
+//  function fingerMove(e) {
+//    target.style.top = e.y + 'px';
+//    target.style.left = e.x + 'px';
+//  }
+//}
+//
+//const leaves = document.getElementsByClassName('drag');
+//
+////ページ読み込み完了を待つ
+//window.addEventListener('load', function(){
+//
+//  for(let i = 0; i < leaves.length; i++) {
+//    draggable(leaves[i]); 
+//  }
+//
+//});
