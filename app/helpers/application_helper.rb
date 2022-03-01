574c6ae814a2a71740c72b77d12ed5851724cd12
kazuma-soon <podradio2020@gmail.com>
2022-03-01 14:59:56 +0900

    Add: OGP & favicon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def default_meta_tags
+    def default_meta_tags
+      {
+        og: {
+          site_name: 'Treasure Hunt',
+          title: '宝探しミニゲーム',
+          description: '宝ゲットであなたの勝ち。うんこゲットであなたの負けです。', 
+          type: 'website',
+          url: request.original_url,
+          image: image_url('ogp.png'),
+          locale: 'ja_JP',
+        }
+      }
+    end
+  end
 end
